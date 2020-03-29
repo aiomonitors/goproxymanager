e0416622344cba2b7a82e24513abb4bacb6144c6
aiomonitors <navrxo@gmail.com>
2020-03-29 02:48:40 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# proxymanager
+
+A simple GoLang implementation of a proxy manager package. Can be initialized with a txt file filled with proxies.
+From the ProxyManager object, you can grab either a random proxy, or sequentially go through the proxies in the object.
+
+### Installation
+```
+go get github.com/aiomonitors/goproxymanager
+```
+
+### Usage
+
+Initialize new object
+```golang
+package main 
+
+import "github.com/aiomonitors/goproxymanager"
+
+func main() {
+    manager, err := proxymanager.NewManager("proxies.txt")
+    if err != nil{
+        panic(err)
+    }
+    
+    //Gets next proxy in the manager
+    next, err := manager.NextProxy()
+    if err != nil{
+        panic(err)
+    }
+   
+    //Gets random proxy in the manager
+    randomProxy, err := manager.RandomProxy()
+    if err != nil{
+        panic(err)
+    }
+}```
---
+++ manager.go
@@ -0,0 +1,63 @@
+package proxymanager
+
+import (
+	"os"
+	"bufio"
+	"errors"
+	"math/rand"
+	"time"
+)
+
+type ProxyManager struct {
+	Proxies []string
+	CurrentIndex int
+}
+
+func NewManager(filename string) (*ProxyManager, error) {
+	file, openErr := os.Open(filename)
+	if openErr != nil {
+		return nil, openErr
+	}
+	defer file.Close()
+	manager := &ProxyManager{Proxies: []string{}, CurrentIndex: 0}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		manager.Proxies = append(manager.Proxies, scanner.Text())
+	}
+	return manager, scanner.Err()
+}
+
+func (p *ProxyManager) LoadProxies(filename string) error {
+	p.Proxies = []string{}
+	file, openErr := os.Open(filename)
+	if openErr != nil {
+		return openErr
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		p.Proxies = append(p.Proxies, scanner.Text())
+	}
+	return scanner.Err()
+}
+
+func (p *ProxyManager) NextProxy() (string, error) {
+	if len(p.Proxies) == 0 {
+		return "", errors.New("ProxyManager.Proxies is empty, load proxies")
+	}
+
+	p.CurrentIndex++ //increases current index by one
+	if p.CurrentIndex > len(p.Proxies) - 1 { 
+		p.CurrentIndex = 0 //sets index to 0 if greater than length
+	}
+	return p.Proxies[p.CurrentIndex], nil
+}
+
+func (p *ProxyManager) RandomProxy() (string, error) {
+	if len(p.Proxies) == 0 {
+		return "", errors.New("ProxyManager.Proxies is empty, load proxies")
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	return p.Proxies[rand.Intn(len(p.Proxies))], nil
+}
